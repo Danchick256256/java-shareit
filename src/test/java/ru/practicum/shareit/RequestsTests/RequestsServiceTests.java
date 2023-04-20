9b76fbbc1bf2d0a54af273baa98fa38f8a9fb7b4
Danchick256256 <danilfpronkin280504@gmail.com>
2023-04-20 16:21:05 +0300

    new version

--- src/test/java/ru/practicum/shareit/RequestsTests/RequestsServiceTests.java
+++ src/test/java/ru/practicum/shareit/RequestsTests/RequestsServiceTests.java
@@ -1,6 +1,7 @@
 package ru.practicum.shareit.RequestsTests;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -26,24 +27,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
+@Slf4j
 class RequestsServiceTests {
     private final RequestsService requestService;
     private final UserService userService;
 
-    @Test
-    @Order(0)
-    @Sql(value = { "/test-schema.sql" })
-    void createUsersTest() {
-        UserDto userCreateDto = UserDto.builder()
-                .name("user")
-                .email("11user@gmail.com")
-                .build();
-        userService.createUser(UserMapper.toUser(userCreateDto));
-    }
 
     @Test
     @Order(1)
+    @Sql(value = { "/test-schema.sql" })
     void createTest() {
+        UserDto userCreateDto = UserDto.builder()
+                .name("user")
+                .email("user@gmail.com")
+                .build();
+        userService.createUser(UserMapper.toUser(userCreateDto));
+
         long userId = 1L;
         RequestsDto incomeDto = RequestsDto.builder()
                 .description("text")
@@ -67,6 +66,18 @@ class RequestsServiceTests {
     void getByIdTest() {
         long requestId = 1L;
         long userId = 1L;
+
+        UserDto userCreateDto = UserDto.builder()
+                .name("user")
+                .email("user@gmail.com")
+                .build();
+        userService.createUser(UserMapper.toUser(userCreateDto));
+
+        RequestsDto incomeDto = RequestsDto.builder()
+                .description("text")
+                .build();
+
+        requestService.createRequest(incomeDto, userId);
 
         Optional<RequestsResponse> requestsResponse = Optional.of(requestService.getRequestById(requestId, userId));
 
@@ -103,48 +114,6 @@ class RequestsServiceTests {
         assertThat(requestsResponseList)
                 .hasSize(1);
         Optional<RequestsResponse> requestsResponse = Optional.of(requestsResponseList.get(0));
-        assertThat(requestsResponse)
-                .isPresent()
-                .hasValueSatisfying(i -> {
-                    assertThat(i).hasFieldOrPropertyWithValue("id", 1L);
-                    assertThat(i).hasFieldOrPropertyWithValue("description", "text");
-                    assertThat(i).hasFieldOrProperty("creationDate");
-                    assertThat(i.getCreationDate()).isNotNull();
-                    assertThat(i).hasFieldOrProperty("items");
-                    assertThat(i.getItems()).hasSize(0);
-                });
-    }
-
-    @Test
-    @Order(4)
-    void getAllTest() {
-        UserDto userCreateDto = UserDto.builder()
-                .name("user")
-                .email("11user@gmail.com")
-                .build();
-        userService.createUser(UserMapper.toUser(userCreateDto));
-        UserDto userCreateDtoSecond = UserDto.builder()
-                .name("user")
-                .email("111user@gmail.com")
-                .build();
-        userService.createUser(UserMapper.toUser(userCreateDtoSecond));
-        long userId = 2L;
-        int from = 0;
-        int size = 10;
-
-        RequestsDto incomeDto = RequestsDto.builder()
-                .description("text")
-                .build();
-
-        requestService.createRequest(incomeDto, userId);
-
-        List<RequestsResponse> requestsResponseList = requestService.getAllRequestsWithSize(userId, from, size);
-
-        assertThat(requestsResponseList)
-                .hasSize(1);
-
-        Optional<RequestsResponse> requestsResponse = Optional.of(requestsResponseList.get(0));
-
         assertThat(requestsResponse)
                 .isPresent()
                 .hasValueSatisfying(i -> {
