1fc2020e4b471e7fa3c53a1fb22432ffbbfa772f
Danchick256256 <danilfpronkin280504@gmail.com>
2023-05-02 22:25:59 +0300

    changed exceptions

--- gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -26,12 +26,12 @@ public class BookingController {
         log.info("create.booking.request");
         /*if (bookingDto.getEnd().isBefore(LocalDateTime.now()))
             throw new BookingBadRequestException("end is before now" + bookingDto.getItemId());*/
-        if (bookingDto.getStart().isBefore(LocalDateTime.now()))
+        /*if (bookingDto.getStart().isBefore(LocalDateTime.now()))
             throw new BookingBadRequestException("start is before now" + bookingDto.getItemId());
         if (bookingDto.getStart().equals(bookingDto.getEnd()))
             throw new BookingBadRequestException("end equals start" + bookingDto.getItemId());
         if (bookingDto.getStart().isAfter(bookingDto.getEnd()))
-            throw new BookingBadRequestException("start is after end" + bookingDto.getItemId());
+            throw new BookingBadRequestException("start is after end" + bookingDto.getItemId());*/
         return bookingClient.addBooking(ownerId, bookingDto);
     }
 
